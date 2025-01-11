5579021fc80c108f617fde4fa3d735f6e8dc8cc7
hgunupuru <hemanth_gunupuru@intuit.com>
2025-01-11 14:31:44 +0530

    HG-01-Initial commit for project setup

---
+++ javamainclass/src/test/java/com/intuit/pavedroad/PrivacyGovernance/MainTest.java
@@ -0,0 +1,22 @@
+package com.intuit.pavedroad.PrivacyGovernance
+
+-road.Privacy-Governance;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * A simple unit test
+ */
+public class MainTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
